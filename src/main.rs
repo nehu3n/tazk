d933f85b0feb6a4d81521e7dec5d7b631213a9ea
nehu3n <developer.nh3@gmail.com>
2025-08-20 00:07:40 -0300

    feat: add basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(name = "Tazk")]
+#[command(version = "0.1.0")]
+#[command(about = "🐕 Lightweight, agnostic, fast and easy task runner.", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Run {
+        #[arg()]
+        name: String,
+    },
+    List {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _cli = Cli::parse();
 }
