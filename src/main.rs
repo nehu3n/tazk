1f06e80abb59f23569306d5390fef1d0b68fa41f
nehu3n <developer.nh3@gmail.com>
2025-08-20 00:44:48 -0300

    feat: add task file detection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod format;
 
+use std::path::{Path, PathBuf};
+
 use clap::Parser;
 
 #[derive(Parser)]
@@ -17,5 +19,37 @@ struct Cli {
 }
 
 fn main() {
-    let _cli = Cli::parse();
+    let cli = Cli::parse();
+
+    let tasks_file = match cli.file {
+        Some(file) => {
+            let path = PathBuf::from(file);
+            if !path.exists() {
+                eprintln!("error: the specified file does not exist: {}", path.display());
+                std::process::exit(1);
+            }
+
+            path
+        }
+        None => detect_tasks_file().unwrap_or_else(|err| {
+            eprintln!("error: {err}");
+            std::process::exit(1);
+        }),
+    };
+
+    println!("{}", tasks_file.display());
+}
+
+fn detect_tasks_file() -> Result<PathBuf, String> {
+    let path = Path::new(".");
+    let expected_files = ["tasks.toml", "tasks.yaml", "tasks.yml", "tasks.json"];
+
+    for expected in expected_files {
+        let file = path.join(expected);
+        if file.exists() {
+            return Ok(file);
+        }
+    }
+
+    Err("no compatible file was found (tasks.toml, tasks.yaml, tasks.yml, tasks.json).".to_string())
 }
